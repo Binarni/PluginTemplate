c3de379f653b98bed5f350be4a192aa8f91ceb31
Binarní <kontakt@binarni.lol>
2024-08-29 19:30:33 +0200

    Foundation with Gradle

---
+++ src/main/java/com/example/template/PluginTemplatePlugin.java
@@ -0,0 +1,11 @@
+package com.example.template;
+
+import org.mineacademy.fo.plugin.SimplePlugin;
+
+public final class PluginTemplatePlugin extends SimplePlugin {
+
+	@Override
+	protected void onPluginStart() {
+	}
+
+}
